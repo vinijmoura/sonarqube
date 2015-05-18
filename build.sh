46722409bc86192f67762741147844f80a8225c1
Stas Vilchik <vilchiks@gmail.com>
2015-05-18 09:35:17 +0200

    build web separately

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 # Parallel executions of maven modules and tests.
 # Half of CPU core are used in to keep other half for OS and other programs.
+cd server/sonar-web && npm install && npm run test && npm run build && cd ../..
 mvn clean install -e -B -T0.5C -DforkCount=0.5C $*
