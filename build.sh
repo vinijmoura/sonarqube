b04cde19eb723e137ec47c7b4c7c6643d54d2ce4
Stas Vilchik <vilchiks@gmail.com>
2015-05-20 10:18:54 +0200

    update build conf using new targets

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 # Parallel executions of maven modules and tests.
 # Half of CPU core are used in to keep other half for OS and other programs.
-cd server/sonar-web && npm install && npm run test && npm run build && cd ../..
+cd server/sonar-web && npm install && npm run build-test && cd ../..
 mvn clean install -e -B -T0.5C -DforkCount=0.5C $*
